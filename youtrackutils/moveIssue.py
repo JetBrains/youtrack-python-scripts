9e9fa1dc1dcea798a1eafbee88caca8407595501
Alexander Buturlinov <imboot85@gmail.com>
2018-02-16 18:06:13 +0300

    fix moveIssue script

--- youtrackutils/moveIssue.py
+++ youtrackutils/moveIssue.py
@@ -6,21 +6,26 @@ if sys.version_info >= (3, 0):
     print("\nThe script doesn't support python 3. Please use python 2.7+\n")
     sys.exit(1)
 
-from youtrack import Issue
+from youtrack import Issue, YouTrackException
 from youtrack.connection import Connection
 
-PREDEFINED_FIELDS = ["summary", "description", "created", "updated", "updaterName", "resolved", "reporterName",
+PREDEFINED_FIELDS = ["summary", "description", "created", "updated",
+                     "updaterName", "resolved", "reporterName",
                      "assigneeName", "priority", "state"]
 
 
 def main():
     try:
-        source_url, source_login, source_password, target_url, target_login, target_password = sys.argv[1:7]
-        source_id, target_project = sys.argv[7:]
-    except:
-        print("Usage: moveIssue source_url source_login source_password target_url target_login target_password source_id target_project")
-        return
-    doMove(source_url, source_login, source_password, target_url, target_login, target_password, source_id, target_project)
+        (source_url, source_login, source_password,
+         target_url, target_login, target_password) = sys.argv[1:7]
+        source_id, target = sys.argv[7:]
+    except (ValueError, IndexError):
+        print("Usage: moveIssue source_url source_login source_password " +
+              "target_url target_login target_password source_id " +
+              "target_project_id_or_issue_id")
+        sys.exit(1)
+    do_move(source_url, source_login, source_password,
+            target_url, target_login, target_password, source_id, target)
 
 
 def check_user(user_login, source, target) :
@@ -28,29 +33,30 @@ def check_user(user_login, source, target) :
         return
     try:
         target.getUser(user_login)
-    except:
+    except YouTrackException:
         source_user = source.getUser(user_login)
-        if not("email" in source_user) :
+        if not("email" in source_user):
             source_user.email = "no_email"
         print(target.importUsers([source_user]))
         print("User %s was created" % user_login)
         return user_login
 
 
-def get_new_issue_id(project_id, target) :
+def get_new_issue_id(project_id, target):
     max_id = 1
     max_number = 100
     while True:
         issues = target.getIssues(project_id, "", max_id, max_number)
         if len(issues) == 0:
             return max_id
-        for issue in issues :
+        for issue in issues:
             issue_id = int(issue.numberInProject)
             if issue_id >= max_id:
                 max_id = issue_id + 1
 
 
-def doMove(source_url, source_login, source_password, target_url, target_login, target_password, source_issue_id, target_project_id) :
+def do_move(source_url, source_login, source_password,
+            target_url, target_login, target_password, source_issue_id, target):
     print("source_url       : " + source_url)
     print("source_login     : " + source_login)
     print("source_password  : " + source_password)
@@ -58,21 +64,30 @@ def doMove(source_url, source_login, source_password, target_url, target_login, 
     print("target_login     : " + target_login)
     print("target_password  : " + target_password)
     print("source_id        : " + source_issue_id)
-    print("target_project   : " + target_project_id)
+
+    if target.find('-') > -1:
+        print("target_id        : " + target)
+        target_project_id, target_issue_number = target.split('-')
+    else:
+        print("target_project_id: " + target)
+        target_project_id = target
+        target_issue_number = None
 
     # connecting
     try:
         target = Connection(target_url, target_login, target_password)
         print("Connected to target url [%s]" % target_url)
     except Exception as ex:
-        print("Failed to connect to target url [%s] with login/password [%s/%s]" % (target_url, target_login, target_password))
+        print("Failed to connect to target url [%s] with login/password [%s/%s]"
+              % (target_url, target_login, target_password))
         raise ex
 
     try:
         source = Connection(source_url, source_login, source_password)
         print("Connected to source url [%s]" % source_url)
     except Exception as ex:
-        print("Failed to connect to source url [%s] with login/password [%s/%s]" % (source_url, source_login, source_password))
+        print("Failed to connect to source url [%s] with login/password [%s/%s]"
+              % (source_url, source_login, source_password))
         raise ex
 
     try:
@@ -81,7 +96,7 @@ def doMove(source_url, source_login, source_password, target_url, target_login, 
         print("Can't connect to target project [%s]" % target_project_id)
         raise ex
 
-    #twin issues
+    # twin issues
     try:
         source_issue = source.getIssue(source_issue_id)
     except Exception as ex:
@@ -90,20 +105,22 @@ def doMove(source_url, source_login, source_password, target_url, target_login, 
 
     target_issue = Issue()
 
-    #import users if needed
-    name_fields = ["reporeterName", "assigneeName", "updaterName"]
-    for field in name_fields :
-        if (field in source_issue) :
+    # import users if needed
+    name_fields = ["reporterName", "assigneeName", "updaterName"]
+    for field in name_fields:
+        if field in source_issue:
             check_user(source_issue[field], source, target)
 
-    target_issue.numberInProject = str(get_new_issue_id(target_project_id, target))
+    if not target_issue_number:
+        target_issue_number = str(get_new_issue_id(target_project_id, target))
+    target_issue.numberInProject = target_issue_number
 
-    #check subsystem
+    # check subsystem
     target_subsystem = None
     try:
         target.getSubsystem(target_project_id, source_issue.subsystem)
         target_subsystem = source_issue.subsystem
-    except:
+    except (YouTrackException, AttributeError):
         pass
     target_issue.subsystem = target_subsystem
     for field in PREDEFINED_FIELDS:
@@ -117,9 +134,9 @@ def doMove(source_url, source_login, source_password, target_url, target_login, 
     else:
         target_issue.type = "Bug"
 
-    #convert custom field
+    # convert custom field
     target_cfs = target.getProjectCustomFields(target_project_id)
-    for cf in target_cfs :
+    for cf in target_cfs:
         cf_name = cf.name
         if cf_name in source_issue:
             target_issue[cf_name] = source_issue[cf_name]
@@ -130,12 +147,16 @@ def doMove(source_url, source_login, source_password, target_url, target_login, 
         check_user(comment.author, source, target)
 
     # attachments
-    print(target.importIssues(target_project_id, target.getProjectAssigneeGroups(target_project_id)[0].name, [target_issue]))
-    for attachment in source_issue.getAttachments() :
-        check_user(attachment.author_login, source, target)
-        #attachment.authorLogin = target_login
-        print(target.createAttachmentFromAttachment("%s-%s" % (target_project_id, target_issue.numberInProject), attachment))
+    print(target.importIssues(
+        target_project_id,
+        target.getProjectAssigneeGroups(target_project_id)[0].name,
+        [target_issue]))
+    for attachment in source_issue.getAttachments():
+        check_user(attachment.authorLogin, source, target)
+        target.createAttachmentFromAttachment(
+            "%s-%s" % (target_project_id, target_issue.numberInProject),
+            attachment)
+
 
 if __name__ == "__main__":
     main()
-  
