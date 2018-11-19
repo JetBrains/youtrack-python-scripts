7123acedb09865085650173c52d7d093683abad3
Alexander Buturlinov <imboot85@gmail.com>
2018-11-19 15:42:03 +0300

    bump version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst')) as f:
 
 setup(
     name='youtrack-scripts',
-    version='0.1.20',
+    version='0.1.21',
     python_requires='>=2.6, <3',
     packages=['youtrackutils',
               'youtrackutils.bugzilla',
